25bfc2198bdddd0c6ab91f23bc8927c5ff06f72b
juliofmendes <33066470+juliofmendes@users.noreply.github.com>
2023-04-19 15:37:53 -0300

    Create exemplo.m

---
+++ exemplo.m
@@ -0,0 +1,4 @@
+#Neste exemplo, estamos usando a biblioteca markdown-it para converter
+* o conteúdo do arquivo .ipc em HTML e exibi-lo na página. 
+# Em seguida, estamos aplicando as classes do Bootstrap aos alertas
+*gerados pela biblioteca markdown-it usando o jQuery. 
